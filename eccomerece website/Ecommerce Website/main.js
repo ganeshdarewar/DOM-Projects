c5cc6e411e5c8ccaf0092c00a2a26486810fd65e
ganeshdarewar <ganeshdarewar@gmail.com>
2024-03-19 17:36:20 +0530

    rename file

--- eccomerece website/Ecommerce Website/main.js
+++
@@ -1,7 +0,0 @@
-import './style.css'
-
-import products from "./api/products.json"
-import { showProductContainer } from './homeProductsCards'
-
-//call the function to display all cards
-showProductContainer(products)
--- eccomerece website/Ecommerce Website/updateCartValue.js
+++
@@ -1,3 +0,0 @@
-export const updateCartValue = (cartProducts)=>{
-    document.getElementById("cartValue").innerHTML=`<i class="fa-solid fa-cart-shopping">${cartProducts.length}</i>`
-}
